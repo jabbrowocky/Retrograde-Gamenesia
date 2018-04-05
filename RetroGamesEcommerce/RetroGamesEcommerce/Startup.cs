b879eca64ab6d0dda7108087fa7672920500265f
Jabbrowocky <mindbull3tz@gmail.com>
2018-04-05 15:07:18 -0500

    initial commit

---
+++ RetroGamesEcommerce/RetroGamesEcommerce/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RetroGamesEcommerce.Startup))]
+namespace RetroGamesEcommerce
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
